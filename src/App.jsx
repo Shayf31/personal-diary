f89da44b654c53fa1f0e38953267c0b3a8b922f3
Shay Fleming <shayfleming1@gmail.com>
2026-05-06 19:09:03 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+
+const App = () => {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">
+        React + TailwindCSS
+      </h1>
+
+      <button className="btn btn-primary bg-red-500">
+        Button
+      </button>
+    </>
+  );
+};
+
+export default App;
