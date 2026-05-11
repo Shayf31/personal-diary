e8321304537d06b5e4d3a52b7f23c17ca2a25b0e
Shay Fleming <shayfleming1@gmail.com>
2026-05-11 17:35:28 +0200

    fixed minor bugs and updated display card format

--- src/components/EntryList.jsx
+++ src/components/EntryList.jsx
@@ -2,25 +2,21 @@ import EntryCard from "./EntryCard";
 
 // Responsible for displaying ALL diary entries
 //
-// Receives TWO props:
+// Receives prop:
 //
 // entries
 // -> array of diary entry objects
 //
-// onViewEntry
-// -> function used to open the detail modal
-const EntryList = ({ entries, onViewEntry }) => {
+const EntryList = ({ entries }) => {
  return (
    <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
    
      {/* .map() loops through EVERY entry in "entries" array */}
      {/* For EACH entry -> create ONE EntryCard */}
-     {/* Pass click handler - send back to app jsx */}
      {entries.map((entry) => (
        <EntryCard
          key={entry.title}
          entry={entry}
-         onViewEntry={onViewEntry}
        />
      ))}
    </section>
